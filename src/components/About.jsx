5796bc280672a13d5a90fa1d835007104317af26
Jack Kiwema <60797039+jackkiwema@users.noreply.github.com>
2022-09-08 12:51:28 +0300

    Updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,7 @@ function About() {
         <div className='w-full md:w-6/12'>
         <SectionTitle>About Me</SectionTitle>
         <p className='text-md text-gray-600 dark:text-gray-300'>
-        Hello, I'm Jack Kiwema, skilled System Administrator in maintaining, supporting networks, systems and 
-                    computing resources. I'm currently exploring supporting legacy systems on the cloud.
+        Hello, I'm Jack Kiwema, skilled System Administrator in maintaining, supporting networks, systems and computing resources. I'm currently exploring supporting legacy systems on the cloud.
         </p>
         <a href="mailto:jackkiwema@gmail.com" className='block mt-3 text-md md:text-lg   text-gray-700 dark:text-gray-300 underline hover:text-indigo-500 dark:hover:text-indigo-500'>jackkiwema@gmail.com</a>
         </div>
